f488d002977e1648c99601f24b4fbb7b52f0fd42
ViVictory11 <162926281+ViVictory11@users.noreply.github.com>
2025-10-07 23:49:52 +0200

    first exceptions implemented

---
+++ Paperless.DAL.Service/Exceptions/DatabaseConnectionException.cs
@@ -0,0 +1,9 @@
+﻿namespace Paperless.DAL.Service.Exceptions
+{
+    public class DatabaseConnectionException : RepositoryException
+    {
+        public DatabaseConnectionException() { }
+        public DatabaseConnectionException(string message) : base(message) { }
+        public DatabaseConnectionException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
---
+++ Paperless.DAL.Service/Exceptions/RepositoryException.cs
@@ -0,0 +1,9 @@
+﻿namespace Paperless.DAL.Service.Exceptions
+{
+    public class RepositoryException : Exception
+    {
+        public RepositoryException() { }
+        public RepositoryException(string message) : base(message) { }
+        public RepositoryException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
