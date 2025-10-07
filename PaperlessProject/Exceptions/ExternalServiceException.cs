54b770ad8747b15bf6c04c7e0aef13ec538db401
ViVictory11 <162926281+ViVictory11@users.noreply.github.com>
2025-10-07 23:49:52 +0200

    first exceptions implemented

---
+++ PaperlessProject/Exceptions/ExternalServiceException.cs
@@ -0,0 +1,12 @@
+﻿namespace PaperlessProject.Exceptions
+{
+    public class ExternalServiceException : Exception
+    {
+        public ExternalServiceException() { }
+
+        public ExternalServiceException(string message) : base(message) { }
+
+        public ExternalServiceException(string message, Exception innerException)
+            : base(message, innerException) { }
+    }
+}
