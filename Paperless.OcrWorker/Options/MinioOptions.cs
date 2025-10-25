92381f2619ceb84843cbd9ec9bbf19ccc08bc928
ViVictory11 <162926281+ViVictory11@users.noreply.github.com>
2025-10-26 00:14:17 +0200

    MinIO integration - bucket and service registration

---
+++ Paperless.OcrWorker/Options/MinioOptions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Paperless.OcrWorker.Options
+{
+    public sealed record MinioOptions(
+        string Endpoint,
+        string AccessKey,
+        string SecretKey,
+        string BucketName,
+        bool UseSSL,
+        int PresignExpirySeconds = 3600);
+}
+
--- Paperless.OcrWorker/Program.cs
+++ Paperless.OcrWorker/Program.cs
@@ -1,9 +1,45 @@
-using Paperless.OcrWorker;
 using Microsoft.Extensions.Hosting;
-
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Minio;
+using Paperless.OcrWorker;
+using Paperless.OcrWorker.Options;
 
 var builder = Host.CreateApplicationBuilder(args);
+
+
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+builder.Logging.AddDebug();
+
+var logger = LoggerFactory.Create(config =>
+{
+    config.AddConsole();
+    config.AddDebug();
+}).CreateLogger("OcrWorker");
+
+logger.LogInformation("Initializing Paperless.OcrWorker...");
+
 builder.Services.AddHostedService<Worker>();
 
+
+builder.Services.Configure<MinioOptions>(builder.Configuration.GetSection("Minio"));
+
+builder.Services.AddSingleton(sp =>
+{
+    var opt = sp.GetRequiredService<IOptions<MinioOptions>>().Value;
+    var client = new MinioClient()
+        .WithEndpoint(opt.Endpoint)
+        .WithCredentials(opt.AccessKey, opt.SecretKey);
+    if (opt.UseSSL)
+        client = client.WithSSL();
+    return client.Build();
+});
+
+logger.LogInformation("MinIO client configured for endpoint {Endpoint}.",
+    builder.Configuration["Minio:Endpoint"] ?? "(none)");
+
 var host = builder.Build();
-host.Run();
+
+logger.LogInformation("Paperless.OcrWorker started.");
+await host.RunAsync();
