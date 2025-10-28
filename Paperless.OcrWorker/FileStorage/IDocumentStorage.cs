56b36c79e04a16e0d9848b22b1aab1d34d386996
ViVictory11 <162926281+ViVictory11@users.noreply.github.com>
2025-10-29 00:44:03 +0100

    minio base implementation done - message gets saved and goes thru ocr

---
+++ Paperless.OcrWorker/FileStorage/IDocumentStorage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Paperless.OcrWorker.FileStorage
+{
+    public interface IDocumentStorage
+    {
+        Task<string> UploadAsync(Stream fileStream, string objectName, string contentType);
+        Task<Stream> DownloadAsync(string objectName);
+        Task DeleteAsync(string objectName);
+    }
+}
